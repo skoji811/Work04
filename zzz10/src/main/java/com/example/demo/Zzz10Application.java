c1ef7b1f55f89a670796fa98aeac1aa213721f51
skoji811 <s.koji811@gmail.com>
2021-05-23 23:02:34 +0900

    first commit

---
+++ zzz10/src/main/java/com/example/demo/Zzz10Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zzz10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zzz10Application.class, args);
+	}
+
+}
